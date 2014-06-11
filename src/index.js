066aa9af2cd33d15d2e1f1d235d1854dc224c030
Leon Mergen <leon@solatis.com>
2014-06-11 10:46:45 +0700

    Adds more flexibility to the types of objects accepted

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 module.exports = function (generator) {
 
+    var objectType = Object.prototype.toString.call(generator);
+
     // If no generator was provided, provide a no-op generator.
     if (!generator) {
         generator = function () { return null; }
+    } else if (objectType == "[object Array]") {
+
+        var i   = 0;
+        var arr = generator;
+
+        generator = function () {
+            if (i < arr.length) {
+                return arr[i++];
+            }
+
+            return null;
+        };
+
+    } else if (objectType != "[object Function]") {
+        // If the generator is a single value, make a one-pass iterator.
+        var done = false;
+        var val  = generator;
+
+        generator = function () {
+            if (done == false) {
+                done = true;
+                return val;
+            } else {
+                return null;
+            }
+        };
     };
 
     return {
--- test/yield-iterator.spec.js
+++ test/yield-iterator.spec.js
@@ -34,3 +34,33 @@ describe('When creating a non-empty iterator', function () {
         }
     });
 });
+
+describe('When creating an iterator of a single value', function () {
+    var iter = new Iterator('foo');
+
+    it('Should return an object on first call', function () {
+        var result = iter.next();
+        chai.expect(result.done).to.be.false;
+        chai.expect(result.value).to.equal('foo');
+    });
+
+    it('Should be done on second call', function () {
+        var result = iter.next();
+        chai.expect(result.done).to.be.true;
+    });
+});
+
+
+describe('When creating an iterator of an array', function () {
+    var arr  = ['foo', 'bar', 'wombat'];
+
+    var iter = new Iterator(arr);
+
+    it('Should iterate over the array', function () {
+        for (var i = 0; i < arr.length; ++i) {
+            var result = iter.next();
+            chai.expect(result.done).to.be.false;
+            chai.expect(result.value).to.equal(arr[i]);
+        }
+    });
+});
